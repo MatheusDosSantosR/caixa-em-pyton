999a8ecc0b499122df26b30acbabbfe9e16abb29
Matheus <matheus2050@gmail.com>
2022-12-03 10:31:17 -0300

    Separação do projeto em modulos

---
+++ main.py
@@ -0,0 +1,22 @@
+import operations
+import utils
+
+
+def main():
+    utils.header()
+    account_auth = operations.auth_account()
+
+    if account_auth:
+        utils.clear()
+        utils.header()
+        option_typed = operations.get_menu_options_typed(account_auth)
+        operations.do_operation(option_typed, account_auth)
+    else:
+        print('Conta inválida')
+            
+while True:
+    main()
+
+    input('Pressione <ENTER> para continuar...')  # pause do programa
+
+    utils.clear()
